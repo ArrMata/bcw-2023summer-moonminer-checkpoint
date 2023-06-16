b8690d243c01bd2fa73d0edfe3ef9e999a01b7b3
Arrington <mataarrington@gmail.com>
2023-06-16 13:35:30 -0600

    basic requirements met

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// #region Global Variables
 
 let playerInfo = {
-    currentCheeseTotal: 0,
+    currentCheeseTotal: 20000,
     clickPower: 1,
     autoPower: 0,
 }
@@ -35,13 +36,54 @@ let autoUpgrades = [
     }
 ]
 
-function addCheese() {
+// #endregion
+
+// #region Main Game Functions
+
+function clickCheese() {
     playerInfo.currentCheeseTotal += playerInfo.clickPower
     drawScreen()
 }
 
+function autoAddCheese() {
+    playerInfo.currentCheeseTotal += playerInfo.autoPower
+    drawScreen()
+}
+
+function purchaseUpgrade(name) {
+    let upgrade = findUpgrade(name)
+    if (playerInfo.currentCheeseTotal >= upgrade.price) {
+        playerInfo.currentCheeseTotal -= upgrade.price
+        upgrade.quantity += 1
+        upgrade.price += Math.floor(.15 * upgrade.price)
+    }
+    updatePlayerStats()
+    drawScreen()
+}
+
+function updatePlayerStats() {
+    let totalClickMods = 0
+    let totalAutoMods = 0
+
+    clickUpgrades.forEach(upgrade => {
+        totalClickMods += upgrade.multiplier * upgrade.quantity
+    })
+
+    autoUpgrades.forEach(upgrade => {
+        totalAutoMods += upgrade.multiplier * upgrade.quantity
+    })
+
+    playerInfo.clickPower = 1 + totalClickMods
+    playerInfo.autoPower = totalAutoMods
+}
+
+// #endregion
+
+// #region Drawing Functions 
+
 function drawScreen() {
     drawPlayerStats()
+    drawPlayerInventory()
 }
 
 function drawPlayerStats() {
@@ -53,4 +95,32 @@ function drawPlayerStats() {
     autoPowerElem.innerText = `+${playerInfo.autoPower}`
 }
 
+function drawPlayerInventory() {
+    const spoonTotalElem = document.getElementById('spoonTotal')
+    const spoonShovelElem = document.getElementById('spoonShovelTotal')
+    const spoonDrillElem = document.getElementById('spoonDrillTotal')
+    const spoonExcavatorElem = document.getElementById('spoonExcavatorTotal')
+    spoonTotalElem.innerText = findUpgrade('Spoon').quantity
+    spoonShovelElem.innerText = findUpgrade('Shovel Spoon').quantity
+    spoonDrillElem.innerText = findUpgrade('Spoon Drill').quantity
+    spoonExcavatorElem.innerText = findUpgrade('Spoon Excavator').quantity
+}
+
+// #endregion
+
+// #region Utility functions
+
+function findUpgrade(name) {
+    let foundUpgrade
+    foundUpgrade = clickUpgrades.find(upgrade => upgrade.name == name)
+    if (foundUpgrade == undefined) {
+        foundUpgrade = autoUpgrades.find(upgrade => upgrade.name == name)
+    }
+    return foundUpgrade
+}
+//#endregion
+
+//#region Function Calls / Game Start
 drawScreen()
+setInterval(autoAddCheese, 3000)
+//#endregion
